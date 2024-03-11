d939a994e3913ac3c92188b72d6da47196ad103f
Ayush Sharma <Ayushsharma014may@gmail.com>
2024-03-12 00:15:31 +0530

    added jodit editor

--- app/newblog/page.tsx
+++ app/newblog/page.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import '../globals.css'
+import NavBar from '../components/navbar/NavBar'
+import TitleInput from '../components/Blog/TitleInput'
+import ImageInput from '../components/Blog/ImageInput'
+import RichTextEditor from '../components/Blog/RichTextEditor'
 
 const NewBlog = () => {
-  return (
-    <div>NewBlog</div>
-  )
+    return (
+        <>
+            <NavBar />
+            <div className='' >
+                <div className='grid justify-center grid-cols-[minmax(300px,1000px)] mt-6 px-5'>
+                    <TitleInput />
+                    <ImageInput />
+                    <RichTextEditor />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default NewBlog
